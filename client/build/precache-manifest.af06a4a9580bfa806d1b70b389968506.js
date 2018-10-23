f6d1cc5391d921303e4a7ff0e78ced52fd0d28a4
Qasim Hafeez <qhafeez26@yahoo.com>
2018-10-23 15:48:40 -0400

    changed folder structure and removed bunc of dependencies

---
+++ client/build/precache-manifest.af06a4a9580bfa806d1b70b389968506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a29b7415724bbd8e34f3b9401beb4eb",
+    "url": "/static/media/appScreenshot.6a29b741.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f09c609dd136871afec",
+    "url": "/static/js/main.6f09c609.chunk.js"
+  },
+  {
+    "revision": "3fd32b014e1527204f62",
+    "url": "/static/js/1.3fd32b01.chunk.js"
+  },
+  {
+    "revision": "6f09c609dd136871afec",
+    "url": "/static/css/main.825a27ba.chunk.css"
+  },
+  {
+    "revision": "e7e7b79e1fa3a8b00514d746790d16ff",
+    "url": "/index.html"
+  }
+];
