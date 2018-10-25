673897e86c6fedaabc3467d4874caeb7feb0f5a6
Qasim Hafeez <qhafeez26@yahoo.com>
2018-10-25 00:05:33 -0400

    edited

--- app.js
+++ app.js
@@ -749,7 +749,9 @@ let server = http.createServer(app,(req,res)=>{
 });
 
 app.get('/*', (req, res) => {
-	console.log("catchall")
+	console.log("catchall");
+	console.log(req.hostname);
+	console.log(req.originalUrl);
 	console.log(path.join(__dirname + '/client', 'build', 'index.html'));
   res.sendFile(path.join(__dirname + '/client', 'build', 'index.html'));
 })
