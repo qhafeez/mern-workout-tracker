6c5dc67624446249ce85a274febe14ee79251ca8
Qasim Hafeez <qhafeez26@yahoo.com>
2018-10-24 21:44:23 -0400

    edited

--- app.js
+++ app.js
@@ -56,6 +56,13 @@ let connection =keys.connection;
 // 	}
 // app.use(cors(options));
 
+
+app.get('/*', (req, res) => {
+  let url = path.join(__dirname, '/client/build', 'index.html');
+  if (!url.startsWith('/app/')) // we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
  
 app.get("/google", passport.authenticate("google",{
 
