2da2e5f87c90362fe36caa16e067ba65dd06205a
Qasim Hafeez <qhafeez26@yahoo.com>
2018-11-10 03:38:17 -0500

    changed logout redirect to /

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.post("/logoutExp",(req,res)=>{
 
 	console.log("logout");
 	req.logout();
-	res.redirect("http://workout-tracker-qh.herokuapp.com");
+	res.redirect("/");
 	// connection.end();
 
 })
