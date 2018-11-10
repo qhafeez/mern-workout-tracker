9c048255265b28e02acacccd3d6bec625908cf7a
Qasim Hafeez <qhafeez26@yahoo.com>
2018-11-10 00:34:42 -0500

    changed redirect to heroku url

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get("/auth/google/redirect", passport.authenticate("google"), (req,res)=>{
 	console.log("isinde redirect");
 	console.log(req.user)
 	
-	res.redirect("http://localhost:3000/#/?token="+req.user.token);
+	res.redirect("http://https://workout-tracker-qh.herokuapp.com?token="+req.user.token);
 	// res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
 });
